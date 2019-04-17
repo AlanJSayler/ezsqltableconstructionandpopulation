3d9f6d2080b571115aad647d88fa083928a03aa8
AlanJSayler <alanjsayler@gmail.com>
2019-04-16 19:57:07 -0500

    add foreign key functionality
    added foreign key stuff

--- main.py
+++ main.py
@@ -5,12 +5,30 @@ import os
 
 SUFFIX = '.json'
 DIR = 'tables'
+OUTPUTFILE = 'newtestoutput'
 
 def createParentheticalSchema(outputfilename):
-    for filename in os.listdir(DIR):
-        if filename.endswith(SUFFIX):
-            getCreateTableQuery(filename)
+    with open(OUTPUTFILE, 'w') as file:
+        for filename in os.listdir(DIR):
+            if filename.endswith(SUFFIX):
+                query = getCreateTableQuery(filename)
+                file.write(query)
+        for filename in os.listdir(DIR):
+            query = getForeignKeyConstraints(filename)
+            if len(query) > 0:
+                file.write(query)
     return
+
+def getForeignKeyConstraints(filename):
+    with open("{}/{}".format(DIR,filename)) as file:
+        doc = json.load(file)
+        filename = filename[:len(filename) - len(SUFFIX)]
+        for field in doc['Fields']:
+            if field['KeyType'] == "Foreign" or field["KeyType"] == 'Combined':
+                return "ALTER TABLE {name} ADD CONSTRAINT FK_{keyname}{name} FOREIGN KEY ({keyname}) REFERENCES {ref}({keyname});\n".format(
+                    name = filename, keyname = field["Name"], ref = field["Reference"])
+            else:
+                return ""
 
 def getCreateTableQuery(filename):
     with open("{}/{}".format(DIR,filename)) as file:
@@ -24,9 +42,9 @@ def getCreateTableQuery(filename):
                 if field["Nullable"] == "True":
                     nullable = ""
                 vals.append("{} {} {}".format(field["Name"], field["Type"], nullable))
-                if field['KeyType'] == "Primary":
-                    vals.append("CONSTRAINT PK_{0} PRIMARY KEY ({0})".format(field["Name"]))
-            query = "CREATE TABLE {name} ({vals});".format(name=filename, vals= ", ".join(vals))
+                if field['KeyType'] == "Primary" or field['KeyType'] == "Combined":
+                    vals.append("CONSTRAINT PK_{0}{1} PRIMARY KEY ({0})".format(field["Name"], filename))
+            query = "CREATE TABLE {name} ({vals});\n".format(name=filename, vals= ", ".join(vals))
             print(query)
             return query
         except:
