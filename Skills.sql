6bb386767f69a136f243f4e9a1db1dc6290346ba
AlanJSayler <alanjsayler@gmail.com>
2019-05-01 17:45:26 -0500

    new skills format

---
+++ Skills.sql
@@ -0,0 +1,24 @@
+INSERT INTO Skills
+(SkillID, SkillCode, Rate, FringeBenefits)
+VALUES
+(1, "LAB", 15, 3);
+
+INSERT INTO Skills
+(SkillID, SkillCode, Rate, FringeBenefits)
+VALUES
+(2, "CAR", 16, 3);
+
+INSERT INTO Skills
+(SkillID, SkillCode, Rate, FringeBenefits)
+VALUES
+(3, "MAS", 17, 3);
+
+INSERT INTO Skills
+(SkillID, SkillCode, Rate, FringeBenefits)
+VALUES
+(4, "IRN", 17, 3);
+
+INSERT INTO Skills
+(SkillID, SkillCode, Rate, FringeBenefits)
+VALUES
+(4, "EQP", 20, 3);
